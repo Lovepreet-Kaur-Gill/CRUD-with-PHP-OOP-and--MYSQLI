353ade3ab0db31db247a28402d89ca5377b793e2
Lovepreet Kaur <preetkaurgill437@gmail.com>
2024-04-13 12:31:43 +0530

    CRUD Project with PHP_OOP

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div class="card-footer">
+                    <h3 class="card-title">Developed by GEETA TECHNICAL HUB</h3>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+include 'header.php';
+?>
+
+                  <div class="card-body">
+<table class="table table-striped table-border table-hover">
+<tr>
+    <th>Serial</th>
+    <th>Name</th>
+    <th>Roll no.</th>
+    <th>Course</th>
+
+</tr>
+<tr>
+    <td>01</td>
+    <td>Lovepreet Kaur</td>
+    <td>1130</td>
+    <td>B.Tech.</td>
+
+</tr>
+<tr>
+    <td>02</td>
+    <td>Lovepreet Kaur</td>
+    <td>1130</td>
+    <td>B.Tech.</td>
+
+</tr><tr>
+    <td>03</td>
+    <td>Lovepreet Kaur</td>
+    <td>1130</td>
+    <td>B.Tech.</td>
+
+</tr><tr>
+    <td>04</td>
+    <td>Lovepreet Kaur</td>
+    <td>1130</td>
+    <td>B.Tech.</td>
+
+</tr>
+<tr>
+    <td>05</td>
+    <td>Lovepreet Kaur</td>
+    <td>1130</td>
+    <td>B.Tech.</td>
+
+</tr>
+</table>
+<a class="btn btn-success" href="create.php">Create</a>
+                  </div>
+                 
+<?php
+include 'footer.php';
+?>
